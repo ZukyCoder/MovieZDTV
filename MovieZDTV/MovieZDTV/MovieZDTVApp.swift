3a3290a33d0f2c8ecc93b59520aab66d1eddc8ac
Mario Heberthardt <zukydeveloper@gmail.com>
2026-04-17 18:05:50 +0200

    Add MovieZDTV tvOS application project
    Initialize the MovieZDTV project with a standard SwiftUI tvOS application template, including the Xcode project configuration, asset catalogs for icons and top shelf images, and initial app and content views.

---
+++ MovieZDTV/MovieZDTV/MovieZDTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieZDTVApp.swift
+//  MovieZDTV
+//
+//  Created by Zuky on 17/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieZDTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
